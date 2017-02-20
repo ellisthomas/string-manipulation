0f48d22ff68517840f9f1ef906839fc0b39af6c6
ellisthomas <ellisthomas@gmail.com>
2017-02-20 12:05:45 -0600

    set up index.html and strings.js ready to go

---
+++ strings.js
@@ -0,0 +1,17 @@
+function reversal() {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
+
