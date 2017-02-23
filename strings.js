e2f3b1ccf396339b2df1970cbfec8b9e5fa24dd6
ellisthomas <ellisthomas@gmail.com>
2017-02-23 00:36:28 -0600

    got fucntion working now on enter still working on click

--- strings.js
+++ strings.js
@@ -1,40 +1,54 @@
-var validChar = /[a-zA-z]/;
-
 var input = document.getElementById("userData");
-
 var button = document.getElementById("returnInput");
+
 button.addEventListener("click", validate);
 button.addEventListener("keypress", validate);
 
-window.addEventListener("keyup",
-	function(e) {
-	if (e.keyCode == 13) {
-    document.getElementById("returnInput").click(testString);
-  }
-});
+document.onkeydown = function() {
+ if (window.event.keyCode === 13) {
+   validate(input.value);
+ }
+};
 
-	manipulateString.userData = document.getElementById("userData").value;
 
-function validate() {
-    if (/""/.test(input)); {
+function validate(e) {
+    if (input == ""); {
         alert("Please enter character");
     }
 
-    if (/^[a-zA-Z]+$/.test(input)); {
-    	alert("Only accepts letters")
+//     if (/^[a-zA-Z]+$/.test(input)); {
+//         alert("Only accepts letters")
+//     }
+// };
+
+
+function reversal(e) {
+    var reverseString = e.split("").reverse().join("");
+    console.log(reverseString);
+    document.getElementById("reverse").innerHTML += reverseString;
+};
+
+
+function alphabits(e) {
+    var alphabits = e.split("").sort().join("");
+    console.log(alphabits);
+    document.getElementById("alpha").innerHTML += alphabits;
+};
+
+
+function palindrome(e) {
+    var reverseString = e.split("").reverse().join("");
+    var palindrome = reverseString.toLowerCase();
+    if (palindrome === e.toLowerCase()) {
+        document.getElementById("pal").innerHTML = "Yay! This string is a Palindrome.";
+    } else {
+        document.getElementById("pal").innerHTML = "Sorry, this string is not a Palindrome.";
     }
 };
 
 
-// function alphabits() {
-
-// }
-
-// function palindrome() {
-
-// }
-
-// function palindrome() {
-	
-// }
-
+function manipulateString(e) {
+    reversal(e);
+    alphabits(e);
+    palindrome(e);
+};
